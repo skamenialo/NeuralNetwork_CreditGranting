0a9e8da909123ec30cb509aa312f33454bae82bd
Daniel Janowski <janowski.d@wp.pl>
2016-01-21 02:05:10 +0100

    Added GUI

--- src/com/skamenialo/creditgranting/RandomNumber.java
+++ src/com/skamenialo/creditgranting/RandomNumber.java
@@ -4,17 +4,17 @@ import java.util.Random;
 
 public class RandomNumber {
 
-    static double nextDouble(double min, double max) {
+    public static double nextDouble(double min, double max) {
         Random rand = new Random();
         return min + rand.nextDouble() * (max - min);
     }
 
-    static int nextInt(int min, int max) {
+    public static int nextInt(int min, int max) {
         Random rand = new Random();
         return rand.nextInt((max - min) + 1) + min;
     }
 
-    static double nextMoney(double min, double max) {
+    public static double nextMoney(double min, double max) {
         double p = nextDouble(nextDouble(min, max / 2), nextDouble(max / 2, max));
         p = nextDouble(p - Math.abs((nextDouble(0, p / 2))), p + Math.abs((nextDouble(0, p / 2))));
         return Math.abs(p);
